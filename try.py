4808b62ba7e38b897509ce97412998efb1a9c063
Mzulhilmi93 <113337039+Mzulhilmi93@users.noreply.github.com>
2022-09-14 13:40:37 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,44 +1,12 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
 
-st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
-
-st.sidebar.header('User Input Parameters')
-
-def user_input_features():
-    Annual_Income = st.sidebar.slider('Annual Income', 10, 200, 10)
-    Spending_Score = st.sidebar.slider('Spending Score', 1, 90, 5)
-    data = {'Annual_Income': Annual_Income_(k$),
-            'Spending_Score': Spending_Score}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df)
-
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
