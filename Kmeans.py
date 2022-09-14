baead250cd418a4e2bbfb527ce69c875a9bfea8c
Mzulhilmi93 <113337039+Mzulhilmi93@users.noreply.github.com>
2022-09-14 11:16:50 +0800

    Add files via upload

---
+++ Kmeans.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
